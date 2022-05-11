e7f8168d472f77365b8b1d8fc05501b9207172ee
ViolaterZ <73651672+ViolaterZ@users.noreply.github.com>
2022-05-11 15:25:02 +0530

    Update logStyles.js

--- modules/logStyles.js
+++ modules/logStyles.js
@@ -1,7 +1,7 @@
 // Console Log Styles
-const basic = (color) => `color: ${color}; font-family: Poppins; font-weight: 600; font-size: 13px; padding-left: -10px; border-radius: 5px;`;
-const modern = (color) => `color: ${color}; font-family: Poppins; font-weight: 600; font-size: 13px; padding: 5px; background: ${color}50; border-radius: 5px;`;
-const flat = (bgColor, textColor) => `color: ${textColor}; font-family: Poppins; font-weight: 600; font-size: 13px; padding: 5px; background: ${bgColor}; border-radius: 5px;`;
+const basic = (color) => `color: ${color}; font-family: sans-serif; font-weight: 600; font-size: 13px; padding-left: -10px; border-radius: 5px;`;
+const modern = (color) => `color: ${color}; font-family: sans-serif; font-weight: 600; font-size: 13px; padding: 5px; background: ${color}50; border-radius: 5px;`;
+const flat = (bgColor, textColor) => `color: ${textColor}; font-family: sans-serif; font-weight: 600; font-size: 13px; padding: 5px; background: ${bgColor}; border-radius: 5px;`;
 
 const logStyles = {
     modern,
